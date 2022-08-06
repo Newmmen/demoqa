8ea86ea4ef23a4e4ba28e55180ee77358b6c4a34
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-08-06 19:54:36 +0300

    fix register user

--- src/test/java/demonqa/TestDemonQa.java
+++ src/test/java/demonqa/TestDemonQa.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import java.io.File;
+
+import static com.codeborne.selenide.CollectionCondition.texts;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
 import static org.openqa.selenium.remote.tracing.EventAttribute.setValue;
 
 public class TestDemonQa {
@@ -25,12 +28,27 @@ public class TestDemonQa {
         $(".react-datepicker__year-select [value='1996']").click();
         $(".react-datepicker__month-select > option").click();
         $(".react-datepicker__week .react-datepicker__day--021").click();
-        $("#userNumber").setValue("547457");
+        $("#userNumber").setValue("5474575555");
         $("#subjectsInput").setValue("Maths").pressEnter();
         $("[for=gender-radio-1]").click();
         $("[for=hobbies-checkbox-1]").click();
         $("#currentAddress").setValue("Lenin st. b.12 a.7");
-        int f =1;
+        $("#uploadPicture").uploadFile(new File("src/test/resources/Photo.jpg"));
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter().pressEnter();
+
+
+        $$("tbody tr").filterBy(text("Student Name")).shouldHave(texts("Gosling Ryan"));
+        $$("tbody tr").filterBy(text("Student Email")).shouldHave(texts("hellomyprepod@gmail.com"));
+        $$("tbody tr").filterBy(text("Gender")).shouldHave(texts("Male"));
+        $$("tbody tr").filterBy(text("Mobile")).shouldHave(texts("5474575555"));
+        $$("tbody tr").filterBy(text("Date of Birth")).shouldHave(texts("21 January,1996"));
+        $$("tbody tr").filterBy(text("Subjects")).shouldHave(texts("Maths"));
+        $$("tbody tr").filterBy(text("Hobbies")).shouldHave(texts("Sports"));
+        $$("tbody tr").filterBy(text("Picture")).shouldHave(texts("Photo.jpg"));
+        $$("tbody tr").filterBy(text("Address")).shouldHave(texts("Lenin st. b.12 a.7 "));
+        $$("tbody tr").filterBy(text("State and City")).shouldHave(texts("NCR Delhi"));
+
 
 
 
