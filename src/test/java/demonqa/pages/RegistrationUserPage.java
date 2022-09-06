bd9f327fed9957b39e4b0d9321a36cc1022f5305
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-09-06 20:57:51 +0300

    add allure

--- src/test/java/demonqa/pages/RegistrationUserPage.java
+++ src/test/java/demonqa/pages/RegistrationUserPage.java
@@ -3,6 +3,7 @@ package demonqa.pages;
 import com.codeborne.selenide.SelenideElement;
 import demonqa.pages.components.CalendarComponent;
 import demonqa.pages.components.ModalResultComponent;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
@@ -28,84 +29,98 @@ public class RegistrationUserPage {
             userCityInput = $("#stateCity-wrapper #react-select-4-input");
 
 
+    @Step("Открывается форма регистрации")
     public RegistrationUserPage openRegisterPage() {
         open("https://demoqa.com/automation-practice-form");
         $(".practice-form-wrapper").$(byText("Student Registration Form")).shouldBe(visible);
         return this;
     }
 
+    @Step("Заполняется имя пользователя")
     public RegistrationUserPage setFirstName(String value) {
         firstNameInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняется фамилия пользователя")
     public RegistrationUserPage setLastName(String value) {
         lastNameInput.setValue(value);
 
         return this;
     }
 
+    @Step("Заполняется email пользователя")
     public RegistrationUserPage setUserEmail(String value) {
         userEmailInput.setValue(value);
         return this;
 
     }
 
+    @Step("Заполняется пол пользователя")
     public RegistrationUserPage setUserGender(String value) {
         userGenderInput.$(byText(value)).click();
         return this;
     }
 
+    @Step("Заполняется номер телефона пользователя")
     public RegistrationUserPage setUserNumber(String value) {
         userNumberInput.setValue(value);
         return this;
     }
 
+    @Step("Заполняется учебный предмет пользователя")
     public RegistrationUserPage setUserSubject(String value) {
         userSubjectInput.setValue(value).pressEnter();
         return this;
 
     }
 
+    @Step("Заполняется хобби пользователя")
     public RegistrationUserPage setUserHobby(String value) {
         userHobbyInput.$(byText(value)).click();
         return this;
     }
 
+    @Step("Заполняется текущий адрес пользователя")
     public RegistrationUserPage setCurrentAddress(String value) {
         userCurrentAddressInput.setValue(value);
         return this;
     }
 
+    @Step("Загружается фото пользователя")
     public RegistrationUserPage uploadUserPhoto(String filename) {
         uploadUserPhotoInput.uploadFromClasspath(filename);
         return this;
     }
 
-
+    @Step("Заполняется штат и город пользователя")
     public RegistrationUserPage setStateAndCity(String state, String city) {
         userStateInput.setValue(state).pressEnter();
         userCityInput.setValue(city).pressEnter();
         return this;
     }
 
+    @Step("Подтверждаем данные нажатием 'Confirm'")
     public RegistrationUserPage clickConfirmButton() {
         $("#userForm #submit").click();
 
         return this;
     }
 
+    @Step("Заполняется день рождения пользователя")
     public RegistrationUserPage setBirthDate(String year, String month, String day) {
         $("#dateOfBirthInput").click();
         calendarComponent.setDate(year, month, day);
         return this;
     }
 
+    @Step("Проверяем открытие окна с данными пользователя")
     public RegistrationUserPage checkModalResultVisible() {
         modalResultComponent.isVisible();
         return this;
     }
 
+    @Step("Проверяем сохранение введенного {0} на сайте")
     public RegistrationUserPage checkResult(String key, String value) {
         modalResultComponent.checkResult(key, value);
         return this;
